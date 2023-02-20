6adc5ecdfd66dcd74de1d42a98c32aad80ac5921
mohamed shegow <shegowmo@gmail.com>
2023-02-20 12:33:20 +0100

    table is functioning & Dyanmic Database as well

--- vidly/src/components/movies.jsx
+++ vidly/src/components/movies.jsx
@@ -8,17 +8,50 @@ class Movies extends Component {
     oldMovies: saveMovie,
   };
 
+
+
   handleDelete(){
 
+  }
+
+  handleTitle(){
+    const {newMovies} = this.state
+    return newMovies.length === 0 ? "There are no Movies!" : `There are ${newMovies.length} movies in the database`
+  };
+
+  handleTable() {
+    const {newMovies} = this.state
+    return(
+    <table class="table">
+      <thead>
+    <tr>
+      <th scope="col">Title</th>
+      <th scope="col">Genre</th>
+      <th scope="col">Stock</th>
+      <th scope="col">Rate</th>
+    </tr>
+  </thead>
+  <tbody>
+  {newMovies.map((movie,i) =>
+  <tr key = {i}>
+      <td key = {i}>{movie.title}</td>
+      <td key = {i}>{movie.genre.name}</td>
+      <td key = {i}>{movie.numberInStock}</td>
+      <td key = {i}>{movie.dailyRentalRate}</td>
+      <button onClick={this.handleDelete()}>Delete</button>
+      </tr>
+      )}
+  </tbody>
+</table>
+    );
   }
 
 
   render() {
     return (
     <div>
-    <h1>Hello World</h1>
-    <ul>{this.state.newMovies.map(movie => <li>{movie.title}</li> )}
-    </ul>
+    <h4>{this.handleTitle()}</h4>
+    <ul>{this.handleTable()}</ul>
     </div>
     );
   }
