b1bb3a1344818ae87a03aa341b84861e5a7164b1
mohamed shegow <shegowmo@gmail.com>
2023-03-11 22:06:05 +0100

    rendering each colulm'

--- vidly/src/components/common/tableBody.jsx
+++ vidly/src/components/common/tableBody.jsx
@@ -3,13 +3,16 @@ import React, { Component } from "react";
 class TableBody extends Component {
 
   render() {
+
+    const { data, columns} = this.props
     return (
       <tbody>
-        <tr>
-        <td></td>
-        </tr>
-      </tbody>
-    );
+        {data.map(item =><tr>
+          {columns.map(column =><td></td>)}
+        </tr> )}
+
+      </tbody>);
+
   }
 }
 
