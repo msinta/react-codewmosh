fc73e3cb4f10c92e367555ef56bbbb6e639ef9bf
mohamed shegow <shegowmo@gmail.com>
2023-03-24 15:20:14 +0100

    added in new routes & H1

--- vidly/src/App.js
+++ vidly/src/App.js
@@ -5,14 +5,19 @@ import MoviesLink from "./components/movieslink";
 import RentalsLink from "./components/rentalslink";
 import CustomersLink from "./components/customerslink";
 import { Switch, Route, Redirect } from "react-router-dom";
+import NotFound from "./components/notFound";
 
 function App() {
   return (
     <div>
       <NavBar />
       <Switch>
-
         <Route path='/' exact component={Movies}/>
+        <Route path='/movies'  component={MoviesLink}/>
+        <Route path='/customers'  component={CustomersLink}/>
+        <Route path='/rentals'  component={RentalsLink}/>
+        <Route path='/not-found'  component={NotFound}/>
+        <Redirect to='not-found'/>
       </Switch>
     </div>
   );
