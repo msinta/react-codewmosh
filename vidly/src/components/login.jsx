663a21b9cad9563584e53f4e4425af6f7d57e306
mohamed shegow <shegowmo@gmail.com>
2023-03-25 13:56:03 +0000

    added in validation for login

--- vidly/src/components/login.jsx
+++ vidly/src/components/login.jsx
@@ -1,36 +1,58 @@
 import React, { Component } from "react";
+import Input from "./common/input";
 
 class LoginForm extends Component {
-  username = React.createRef();
+  state = {
+    account: { username: "", password: "" },
+    errors: {},
+  };
+  handleChange = ({ currentTarget: input }) => {
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account });
+  };
 
-  // componentDidMount() {
-  //   this.username.current.focus();
-  // }
+  validate = () => {
+    const errors = {};
+
+    const { account} = this.state
+    if(account.username.trim() === '')
+      errors.username = 'Username is required.'
+    if(account.password.trim() === '')
+      errors.password = 'Password is required.'
+
+
+    return { username: "Username is required" };
+
+  };
 
   handleSubmit = (e) => {
     e.preventDefault();
-    const username = this.username.current.value;
+    const errors = this.validate();
+    this.setState({ errors });
+    if (errors) return;
     console.log("Submitted");
   };
 
   render() {
+    const { account } = this.state;
     return (
       <div className="container">
         <h1>Login</h1>
         <form onSubmit={this.handleSubmit} action="">
+          <Input
+            name="username"
+            value={account.username}
+            label="Username"
+            onChange={this.onChange}
+          />
           <div className="form-group">
-            <label htmlFor="username">Username</label>
-            <input
-              autoFocus
-              ref={this.username}
-              id="username"
-              type="text"
-              className="form-control"
+            <Input
+              name="password"
+              value={account.password}
+              label="Password"
+              onChange={this.onChange}
             />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input id="password" type="text" className="form-control" />
           </div>
           <button className="btn btn-primary">Login</button>
         </form>
