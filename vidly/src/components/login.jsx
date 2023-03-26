5988d3c7b38c77ab957712904e2b7b13c5e8aa45
mohamed shegow <shegowmo@gmail.com>
2023-03-26 14:26:52 +0100

    adding in new reusable components

--- vidly/src/components/login.jsx
+++ vidly/src/components/login.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Input from "./common/input";
+import Joi from "joi-browser";
 
 class LoginForm extends Component {
   state = {
@@ -7,22 +8,23 @@ class LoginForm extends Component {
     errors: {},
   };
 
-  validateProperty = ({name, value}) =>{
+  schema = {
+    username: Joi.string().required().label("Username"),
+    password: Joi.string().required().label("Password"),
+  };
 
-    if(name === 'username'){
-      if(value.trim()=== '') return 'Username is required'
-    }
-    if(name === 'password'){
-      if(value.trim()=== '') return 'Password is required'
-    }
-  }
-
+  validateProperty = ({ name, value }) => {
+    const obj = {[name]: value};
+    const schema = {[name]: this.schema[name]};
+    const {error} = Joi.validate(obj, schema);
+    return error ? error.details[0].message : null;
+  };
 
   handleChange = ({ currentTarget: input }) => {
-    const errors = {...this.state.errors};
+    const errors = { ...this.state.errors };
     const errorMessage = this.validateProperty(input);
 
-    if(errorMessage) errors[input.name] = errorMessage;
+    if (errorMessage) errors[input.name] = errorMessage;
     else delete errors[input.name];
 
     const account = { ...this.state.account };
@@ -31,15 +33,13 @@ class LoginForm extends Component {
   };
 
   validate = () => {
+    const options = { abortEarly: false };
+    const { error } = Joi.validate(this.state.account, this.schema, options);
+    if (!error) return null;
+
     const errors = {};
-
-    const { account } = this.state;
-    if (account.username.trim() === "")
-      errors.username = "Username is required.";
-    if (account.password.trim() === "")
-      errors.password = "Password is required.";
-
-    return Object.keys(errors).length === 0 ? null : errors;
+    for (let item of error.details) errors[item.path[0]] = item.message;
+    return errors;
   };
 
   handleSubmit = (e) => {
@@ -71,10 +71,10 @@ class LoginForm extends Component {
               error={errors.password}
               onChange={this.handleChange}
             />
-
-
           </div>
-          <button className="btn btn-primary">Login</button>
+          <button
+          disabled={this.validate()}
+          className="btn btn-primary">Login</button>
         </form>
       </div>
     );
