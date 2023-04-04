d44493fc0241940ebe290e1fbe794ffee7b15454
mohamed shegow <shegowmo@gmail.com>
2023-04-04 16:27:52 -0400

    mapping genre id

--- vidly/src/App.js
+++ vidly/src/App.js
@@ -4,19 +4,18 @@ import Rentals from "./components/rentals";
 import Customers from "./components/customers";
 import { Switch, Route, Redirect } from "react-router-dom";
 import NotFound from "./components/notFound";
-import MovieDetails from "./components/moviedetails";
 import LoginForm from "./components/login";
 import RegisterForm from "./components/register";
 import "./App.css";
-import newMovie from "./components/moviesnew";
+import MovieForm from "./components/moviesform";
 
 function App() {
   return (
     <div>
       <NavBar />
       <Switch>
-        <Route path="/movies/new" exact component={newMovie} />
-        <Route path="/movies/:id" component={MovieDetails} />
+        <Route path="/movies/new" component={MovieForm} />
+        <Route path="/movies/:id" component={MovieForm} />
         <Route path="/movies" component={Movies} />
         <Route path="/customers" component={Customers} />
         <Route path="/rentals" component={Rentals} />
