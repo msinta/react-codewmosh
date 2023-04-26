343e4ec4f0ce20b47026abcfc9e99436a6b6c4cf
mohamed shegow <shegowmo@gmail.com>
2023-04-26 15:05:49 -0700

    working on new button ship

--- button-vlite/button/src/App.tsx
+++ button-vlite/button/src/App.tsx
@@ -1,20 +1,20 @@
 import { useState } from 'react'
 import './App.css'
-import ButtonClass from './assets/components/button'
 import Alert from './assets/components/Alert'
+import ButtonClass from './assets/components/Button'
 
 function App() {
   const [count, setCount] = useState(0)
 
   const alertMessage = () => {
-    return <div> <Alert>Clicked Event</Alert></div>
+    return
   }
 
   return (
     <>
       <div>
       <Alert color="primary">Clicked Event</Alert>
-       <ButtonClass color="primary" onClick={() => alertMessage}>My Button</ButtonClass>
+       <ButtonClass color="primary" onClick={() => alertMessage()}>My Button</ButtonClass>
       </div>
     </>
   )
